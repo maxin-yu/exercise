2720f750caaa90786561274c2ccff3ef38baadcd
maxin-yu <67947267+maxin-yu@users.noreply.github.com>
2020-08-30 17:24:08 +0800

    判断矩形是否包含点

---
+++ 判断矩形是否包含点 .c
@@ -0,0 +1,17 @@
+ 
+#include<stdio.h>
+int main()
+{
+	double a,b;
+	double x1,x2,x3,x4,y1,y2,y3,y4;
+	if( (x1-x2)*(y1-b)-(y1-y2)*(x1-a)>=0&&(x3-x4)*(y3-b)-(x3-a)*(y3-y4)>=0 ){
+		printf("Yes");
+	}
+	else{
+		printf("No");
+	}
+	
+	
+	return 0;
+ }
+
